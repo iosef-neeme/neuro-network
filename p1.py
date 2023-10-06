accc06d50c05177b4f1157d1981f0359b3be85ec
iosef-neeme <oaie_007@yahoo.com>
2023-10-06 21:09:09 +0300

    first virsion that includes a "object layer"

--- p1.py
+++ p1.py
@@ -11,7 +11,14 @@ class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
-    def forward(self):
-        pass
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) +self.biases
 
-print(0.1*np.random.randn(4, 3))
+layer1 = Layer_Dense(4,5)
+layer2 = Layer_Dense(5,2)
+
+layer1.forward(X)
+print(layer1.output)
+layer2.forward(layer1.output)
+print("================================")
+print(layer2.output)
