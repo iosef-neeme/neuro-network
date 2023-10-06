bb29a77dbac49f095c1ca61a20a35da4209d1446
iosef-neeme <oaie_007@yahoo.com>
2023-10-06 21:26:10 +0300

    .

--- p1.py
+++ p1.py
@@ -5,8 +5,14 @@ X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
 
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
+'''
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
@@ -22,3 +28,4 @@ print(layer1.output)
 layer2.forward(layer1.output)
 print("================================")
 print(layer2.output)
+'''
